dfa059b2f2d7ffbc2b23c6866f37e811a7267bdd
Corbies <1557655749@qq.com>
2018-03-08 10:38:13 +0800

    第一次修改

---
+++ src/main/java/com/example/demo/controller/Book.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book {
+    @Value("${book.name}")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
